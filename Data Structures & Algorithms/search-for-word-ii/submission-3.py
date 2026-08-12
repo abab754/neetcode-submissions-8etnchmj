229836dd76a7ea7846f81cad2b6791de935f021a
Abhiram Banda <110752285+abab754@users.noreply.github.com>
2026-08-12 15:54:14 -0400

    Add: search-for-word-ii - submission-3

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-3.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self):
+        self.end = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def add(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Node()
+            cur = cur.children[c]
+        cur.end = True
+    
+    def search(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.end
+
+    def prefix(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        res = []
+        trie = Trie()
+        for word in words:
+            trie.add(word)
+
+        visit = set()
+        rows = len(board)
+        cols = len(board[0])
+        def dfs(r, c, cur):
+            if r not in range(rows) or c not in range(cols) or (r,c) in visit:
+                return
+            cur += board[r][c]
+            if not trie.prefix(cur):
+                return 
+            if trie.search(cur) and cur not in res:
+                res.append(cur)
+            visit.add((r,c))
+            dfs(r+1, c, cur)
+            dfs(r-1, c, cur)
+            dfs(r, c+1, cur)
+            dfs(r, c-1, cur)
+            visit.remove((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, "")
+        
+        return list(res)
