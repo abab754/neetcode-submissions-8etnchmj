e2aea4d3a54932f3b4599cf50567cc1f0db7dec3
Abhiram Banda <110752285+abab754@users.noreply.github.com>
2026-08-12 00:25:41 -0400

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self):
+        self.end = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def add(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Node()
+            cur = cur.children[c]
+        cur.end = True
+
+    def search(self, word, root):
+        cur = root
+        for i in range(len(word)):
+            if word[i] == ".":
+                for child in cur.children.values():
+                    if i + 1 >= len(word):
+                        return child.end
+                    if self.search(word[i+1:], child):
+                        return True
+            if word[i] not in cur.children:
+                return False
+            cur = cur.children[word[i]]
+        return cur.end
+
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = Trie()
+
+    def addWord(self, word: str) -> None:
+        self.trie.add(word)
+
+    def search(self, word: str) -> bool:
+        return self.trie.search(word, self.trie.root)
+        
