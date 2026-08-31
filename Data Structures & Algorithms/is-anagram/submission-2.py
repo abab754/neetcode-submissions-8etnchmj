d3b1c7fb31fb6819e169c5c33b9ca45e551eed2d
Abhiram Banda <110752285+abab754@users.noreply.github.com>
2026-08-31 16:14:49 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hm1 = defaultdict(int)
+        hm2 = defaultdict(int)
+        for c in s:
+            hm1[c] += 1
+        
+        for c in t:
+            hm2[c] += 1
+        
+        return hm1 == hm2
